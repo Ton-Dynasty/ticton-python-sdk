6833cc11bb9405f1d6e7a7d38e8959635eeaf1c1
grace0950 <pinyan0950@gmail.com>
2024-02-12 21:46:13 +0800

    Bump version to 0.1.9

--- ticton/__init__.py
+++ ticton/__init__.py
@@ -2,7 +2,7 @@ from .arithmetic import FixedFloat, to_token, token_to_float
 from .client import TicTonAsyncClient
 from .toncenter import TonCenterClient, ToncenterWrongResult
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 __all__ = [
     "FixedFloat",
