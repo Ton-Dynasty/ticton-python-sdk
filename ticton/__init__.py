370f4b2edfc43b6d40c1630fb399047db21b5841
alan890104 <alan890104@gmail.com>
2024-02-18 19:38:27 +0800

    Bump version to 0.0.13

--- ticton/__init__.py
+++ ticton/__init__.py
@@ -1,7 +1,7 @@
 from .arithmetic import FixedFloat, to_token, token_to_float
 from .client import TicTonAsyncClient
 
-__version__ = "0.1.12"
+__version__ = "0.1.13"
 
 __all__ = [
     "FixedFloat",
