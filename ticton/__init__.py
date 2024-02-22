9b591595d8143c37ef8845dd3a63dd667e991c66
alan890104 <alan890104@gmail.com>
2024-02-22 17:04:59 +0800

    fix: Add DryRunResult to TicTonAsyncClient

--- ticton/__init__.py
+++ ticton/__init__.py
@@ -1,5 +1,5 @@
 from .arithmetic import FixedFloat, to_token, token_to_float
-from .client import TicTonAsyncClient
+from .client import DryRunResult, TicTonAsyncClient
 
 __version__ = "0.1.15"
 
@@ -10,4 +10,5 @@ __all__ = [
     "TicTonAsyncClient",
     "TonCenterClient",
     "ToncenterWrongResult",
+    "DryRunResult",
 ]
